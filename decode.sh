af5c0a8a9773954229c0d0fee022668dd0715a26
shuai.lou <hdchild@163.com>
2016-10-27 17:02:55 +0800

    Postfix decode.sh remove encode=base64 when decode.

--- decode.sh
+++ decode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 awk '{ if(match($0,/<SQLTEXT encode="base64">.*<\/SQLTEXT>/))
-{printf("%s", substr($0,1,RSTART+24));
+{printf("%s<SQLTEXT>", substr($0,1,RSTART-1));
 cmd=sprintf("echo -n %s|base64 -d", substr($0,RSTART+25,RLENGTH-35));
 system(cmd);
 printf("</SQLTEXT>\n");
